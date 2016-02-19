63c012303d0bcb944d6a178961529a3e495742be
xiaodong.yan <xiaodong.yan@tubemogul.com>
2016-02-19 17:05:55 +0800

    update code

--- python_source/python_source/spiders/manong_spider.py
+++ python_source/python_source/spiders/manong_spider.py
@@ -1,10 +1,21 @@
 import scrapy
 
 
-class manong_spider(scrapy.Spider):
+class manong(scrapy.Spider):
     name = "manong"
-    allowed_domains = ["tubemogul.com"]
-    start_urls = ["http://www.tubemogul.com"]
+    allowed_domains = ["weekly.manong.io/"]
+    start_urls = ["http://weekly.manong.io/issues/"]
+    for number in range(1, 106):
+        start_urls.append("http://weekly.manong.io/issues/{0}".format(number))
 
     def parse(self, response):
         print "this is response", response
+        # filename = "./files/" + response.url.split('/')[-1] + ".html"
+        filename = "./files/ " + "manong_python_reource.html"
+
+        p = response.xpath('//h4').extract()
+        with open(filename, "a+") as f:
+            for item in p:
+                if u"python" in item or u"Python" in item:
+                    f.write(item.encode("utf-8"))
+                    f.write("\n")
