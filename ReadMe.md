78949d793e12ff0240eb1c27ba35eddf6cb9a1bf
xiaodong.yan <xiaodong.yan@tubemogul.com>
2016-11-12 15:34:41 +0800

    [add] add readme md file

---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# How to get the python resource from manong http://weekly.manong.io/issues/
+## Install the python scrapy framewrok 
+## use python version 2.7
+## cd python_resource folder
+## run command "scrapy crawl manong"
+**Change the key word in code to get other resource, such java or spark**
+
--- python_source/python_source/spiders/manong_spider.py
+++ python_source/python_source/spiders/manong_spider.py
@@ -1,20 +1,29 @@
 import scrapy
+from ..items import PythonSourceItem
 
 
 class manong(scrapy.Spider):
     name = "manong"
     allowed_domains = ["weekly.manong.io/"]
     start_urls = ["http://weekly.manong.io/issues/"]
-    for number in range(1, 106):
+    issue_number = 143 # Issue number should be less than current issue number
+    for number in range(1, issue_number):
         start_urls.append("http://weekly.manong.io/issues/{0}".format(number))
 
+    filename = "./files/" + "manong_python_reource.html"
     def parse(self, response):
         print "this is response", response
         # filename = "./files/" + response.url.split('/')[-1] + ".html"
-        filename = "./files/ " + "manong_python_reource.html"
+
 
         p = response.xpath('//h4').extract()
-        with open(filename, "a+") as f:
+        item_dic = PythonSourceItem()
+        # items = []
+        # for i in p:
+        #     item_dic['desc'] = unicode(i)
+        #     items.append(item_dic)
+        # return items
+        with open(self.filename, "a+") as f:
             for item in p:
                 if u"python" in item or u"Python" in item:
                     f.write(item.encode("utf-8"))
